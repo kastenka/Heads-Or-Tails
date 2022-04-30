cf23f1324ccf2febd95985bbbdde5576815b3cff
Kastenka <kastenka.whatsup@gmail.com>
2022-04-30 16:18:11 +0300

    add runtime.txt

--- headsortails/headsortails/settings.py
+++ headsortails/headsortails/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -133,6 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TOKEN = '5366536301:AAHF8AvZI8ot1NeQ4zQFnvf0M6Co3zJHsog'
 
-
 django_heroku.settings(locals())
-
