e8117afd11a2cdb032f508cd4103b86c59c30ab6
Kastenka <kastenka.whatsup@gmail.com>
2022-05-06 10:59:29 +0300

    edit settings

--- headsortails/telegrambot/static_text_en.py
+++ headsortails/telegrambot/static_text_en.py
@@ -1,12 +1,21 @@
-WRONG_INPUT = "The command is incorrect\n Follow /help to get help"
+WRONG_INPUT = "The command is incorrect\n" \
+              "Follow /help to get help"
+
 
 HEADS = "HEADS"
+
 TAILS = "TAILS"
 
-WON = "You won.\nFollow /gamehistory to view the game history"
-LOST = "You lost.\nFollow /gamehistory to view the game history"
+
+WON = "You won.\n" \
+      "Follow /gamehistory to view the game history"
+
+LOST = "You lost.\n" \
+       "Follow /gamehistory to view the game history"
+
 
 NOT_ENOUGH_COINS = "The bet is higher than your number of coins!"
+
 
 HELP_MESSAGE = "This is a bot game called 'Heads Or Tails'\. " \
                "If the player guesses the result, he gets a doubled prize\. Otherwise he loses his bet\.\n" \
@@ -18,5 +27,6 @@ HELP_MESSAGE = "This is a bot game called 'Heads Or Tails'\. " \
                "/leaderboard \- Get the 10 best players with the highest number of coins\n" \
                "/gamehistory \- Get your history of games\n" \
                "/help \- What can this bot do?"
+
 
 UNKNOWN_COMMAND = "I don't know such a command"
